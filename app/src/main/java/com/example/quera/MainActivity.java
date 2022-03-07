9c275063b26b37e4fc9e190640edafaf1e868ab2
MahtaFetrat <77fetrat@gmail.com>
2022-03-07 10:44:56 +0330

    change login activity to fragment

--- app/src/main/java/com/example/quera/MainActivity.java
+++ app/src/main/java/com/example/quera/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.quera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*TODO: To be replaced with conditional navigation to dashboard/login fragment based on sign in information loaded at startup
+           read https://developer.android.com/guide/navigation/navigation-conditional for more*/
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_activity_fragment_layout, new LoginFragment()).commit();
     }
 }
