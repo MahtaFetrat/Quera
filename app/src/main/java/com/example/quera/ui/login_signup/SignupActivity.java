7c4a5ac783a164f3eaaf8e3acbb2490e656e08a7
MahtaFetrat <77fetrat@gmail.com>
2022-03-08 21:59:57 +0330

    connect activities

--- app/src/main/java/com/example/quera/ui/login_signup/SignupActivity.java
+++ app/src/main/java/com/example/quera/ui/login_signup/SignupActivity.java
@@ -1,10 +1,16 @@
 package com.example.quera.ui.login_signup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.quera.MainActivity;
 import com.example.quera.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -12,5 +18,44 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        setDefaultTab();
+        setTabLayout();
+    }
+
+    private void setDefaultTab() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.signupTabFragmentContainer, new StudentSignupFragment())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit();
+    }
+
+    private void setTabLayout() {
+        ((TabLayout) findViewById(R.id.signupTabLayout)).addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment = new StudentSignupFragment();
+                if (tab.getPosition() == 1) {
+                    fragment = new InstructorSignupFragment();
+                }
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.signupTabFragmentContainer, fragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+    }
+
+    public void navToLoginForm(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
