0b5e26febd1264c1673d3d16d3a219dbf2c2aa21
exampleforgit <sadegh2040mah@gmail.com>
2022-03-19 10:36:49 +0330

    Answers & Grade v.01 It has some bugs that I'll fix them when class is finished

---
+++ app/src/main/java/com/example/quera/view/assignments/AnswersFragment.java
@@ -0,0 +1,40 @@
+package com.example.quera.view.assignments;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quera.R;
+import com.example.quera.model.Answer;
+import com.example.quera.model.Assignment;
+
+import java.util.ArrayList;
+
+public class AnswersFragment extends AppCompatActivity {
+    protected Assignment enteredAssignment;
+    protected ArrayList<Answer> classAnswer;
+    protected ArrayList<String> answers;
+    protected ArrayList<Float> grades;
+    private RecyclerView recyclerView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_answers);
+
+        classAnswer = enteredAssignment.getAnswers();
+
+        for (Answer answer : classAnswer) {
+            answers.add(answer.getAnswer());
+            grades.add(answer.getGrade());
+        }
+
+        AnswersAdapter answersAdapter = new AnswersAdapter(this, answers.toArray(), grades.toArray());
+        recyclerView.setAdapter(answersAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
