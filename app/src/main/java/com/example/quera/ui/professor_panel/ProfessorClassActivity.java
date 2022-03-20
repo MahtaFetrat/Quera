7cacfec84c020f95a34ae5ee4b49804ce8472f94
exampleforgit <sadegh2040mah@gmail.com>
2022-03-20 10:14:29 +0330

    some bugs

--- app/src/main/java/com/example/quera/ui/professor_panel/ProfessorClassActivity.java
+++ app/src/main/java/com/example/quera/ui/professor_panel/ProfessorClassActivity.java
@@ -1,6 +1,6 @@
 package com.example.quera.ui.professor_panel;
 
-import android.content.DialogInterface;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,89 +21,33 @@ import com.example.quera.model.Professor;
 
 import java.util.ArrayList;
 
-//public class ProfessorClassActivity extends BaseActivity {
-//    ClassController controller = MainActivity.classController;
-//
-//    Professor professor;
-//    Course clas;
-//    TextView classNameTextView;
-//    TextView professorNameTextView;
-//    TextView assignmentsTextView;
-//    EditText assignmentEditText;
-//    TextView messageTextView;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_student_class);
-//
-//        Intent intent = getIntent();
-//        String username = intent.getStringExtra("username");
-//        String className = intent.getStringExtra("className");
-//
-//        classNameTextView = findViewById(R.id.classNameStudentClassTextView);
-//        professorNameTextView = findViewById(R.id.professorNameStudentClassTextView);
-//        assignmentsTextView = findViewById(R.id.studentClassAssignmentsTextView);
-//        assignmentEditText = findViewById(R.id.assignmentNameStudentClass);
-//        messageTextView = findViewById(R.id.studentClassMessage);
-//
-//        this.professor = MainActivity.professorPanelController.getProfessorByUsername(username);
-//        this.clas = controller.getUserClassByName(professor, className);
-//        classNameTextView.setText(className);
-//        professorNameTextView.setText(professor.getName());
-//        assignmentsTextView.setText(controller.getClassAssignments(this.clas));
-//    }
-//
-//    public void navToAssignment(View view) {
-//        if (controller.getClassAssignmentByName(clas, assignmentEditText.getText().toString()) == null) {
-//            messageTextView.setTextColor(Color.RED);
-//        } else {
-//                    /* TODO go to assignment page
-//                    Intent intent = new Intent(...)
-//                    ...
-//                     */
-//        }
-//    }
-//
-//    public void createAssignment(View view) {
-//        /* TODO go to create assignment page
-//                Intent intent = new Intent(...)
-//                 */
-//    }
-//}
-
 public class ProfessorClassActivity extends AppCompatActivity {
     ClassController controller = MainActivity.classController;
 
     protected Course course;
     protected Professor professor;
-    protected ArrayList<Assignment> classAssignments;
     protected ArrayList<String> assignmentsName = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private TextView classNameTextView;
-    private TextView professorNameTextView;
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_professor_class);
 
-        recyclerView = findViewById(R.id.ProfessorAssignmentsRecyclerView);
-        classNameTextView = findViewById(R.id.professorClassActivityClassName);
-        professorNameTextView = findViewById(R.id.professorClassActivityProfessorName);
+        RecyclerView recyclerView = findViewById(R.id.ProfessorAssignmentsRecyclerView);
+        TextView classNameTextView = findViewById(R.id.professorClassActivityClassName);
+        TextView professorNameTextView = findViewById(R.id.professorClassActivityProfessorName);
 
 
         Intent intent = getIntent();
         course = controller.getClassByName(intent.getStringExtra("className"));
-        classAssignments = course.getAssignments();
+        assignmentsName = course.getAssignmentIds();
 
-        for (Assignment assignment : classAssignments) {
-            assignmentsName.add(assignment.getName());
-        }
+
 
         if (assignmentsName.size() != 0) {
-            ProfessorAssignmentsAdapter professorAssignmentsAdapter = new ProfessorAssignmentsAdapter(this, assignmentsName.toArray(new String[assignmentsName.size()]));
+            ProfessorAssignmentsAdapter professorAssignmentsAdapter = new ProfessorAssignmentsAdapter(this, assignmentsName.toArray(new String[0]));
             recyclerView.setAdapter(professorAssignmentsAdapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }
@@ -122,18 +66,14 @@ public class ProfessorClassActivity extends AppCompatActivity {
         final EditText nameInputField = new EditText(this);
         inputAssignmentName.setView(nameInputField);
 
-        inputAssignmentName.setPositiveButton("Create", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                String name = nameInputField.getText().toString();
-                Assignment assignment = new Assignment(name, course.getName());
-                finish();
-                startActivity(getIntent());
-            }
+        inputAssignmentName.setPositiveButton("Create", (dialog, whichButton) -> {
+            String name = nameInputField.getText().toString();
+            new Assignment(name, course.getName());
+            finish();
+            startActivity(getIntent());
         });
 
-        inputAssignmentName.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-            }
+        inputAssignmentName.setNegativeButton("Cancel", (dialog, whichButton) -> {
         });
 
         inputAssignmentName.show();
